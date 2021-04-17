862172f923636d620aa5043be3a9dfe7826c6439
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-04-18 00:45:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Item from './components/Item'
+
+import Accessories from './assets/Desktop-Accessories.jpg'
+import ModelS from './assets/Desktop-ModelS.jpeg'
+import Model3 from './assets/Desktop-Model3.jpeg'
+import ModelX from './assets/Desktop-ModelX.jpeg'
+import ModelY from './assets/Desktop-ModelY.jpeg'
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg'
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title='Lowest Cost Solar panels in America'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={SolarPanels}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+
+        <Item
+          title='Model S'
+          desc='$69,420'
+          descLink=''
+          backgroundImg={ModelS}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+
+        <Item
+          title='Model 3'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={Model3}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+
+        <Item
+          title='Model X'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelX}
+          leftBtnTxt='ORDER ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+
+        />
+
+        <Item
+          title='Model Y'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelY}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+
+        <Item
+          title='Accessories'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={Accessories}
+          leftBtnTxt='SHOP NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+        />
+      </div>
     </div>
   );
 }
